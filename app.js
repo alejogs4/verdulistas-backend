42d47fe2f418bbe14a8785542ac91a9b4930c065
alejogs4 <alejogs4@gmail.com>
2019-08-25 18:24:06 +0000

    Resuelve errores de gql

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ const setupGraphql = require('./graphql')
 
 setupGraphql(app);
 
+// Express
+const access = (req, res, next) => {
+	  res.header('Access-Control-Allow-Origin', '*')
+	  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	  res.header('Access-Control-Allow-Headers', 'Content-Type')
+	  next()
+}
+
+ app.use(access)
+
 module.exports = app;
