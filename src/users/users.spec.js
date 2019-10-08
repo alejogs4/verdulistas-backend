e6b69f12334c68046ee19ac776d3435b9c843a1d
alejogs4 <alejogs4@gmail.com>
2019-10-08 16:32:38 -0500

    Add tests for login

--- src/users/users.spec.js
+++ src/users/users.spec.js
@@ -10,4 +10,46 @@ describe('Test for user resolvers', () => {
     expect(Array.isArray(users)).toBe(true);
     expect(userModel.getAll).toBeCalled();
   });
+
+  it('Should fail if there is not a returned user in login', async () => {
+    const userInfo = { email: 'alejogs4@gmail.com', password: '123456' };
+    const userModel = {
+      signIn: jest.fn().mockResolvedValue(null),
+    };
+
+    const auth = {
+      encryptPassword: jest.fn().mockImplementation(() => userInfo),
+    };
+
+    try {
+      await usersResolvers.signIn(null, userInfo, { userModel, auth });
+    } catch (error) {
+      expect(error.message).toBe('Either email or password are incorrect');
+      expect(userModel.signIn).toHaveBeenCalled();
+      expect(userModel.signIn).toHaveBeenCalledWith(userInfo.email, userInfo.password);
+      expect(auth.encryptPassword).toHaveBeenCalled();
+    }
+  });
+
+  it('Should return a user with his session token', async () => {
+    const userInfo = { email: 'alejogs4@gmail.com', password: '123456' };
+    const userModel = {
+      signIn: jest.fn().mockResolvedValue(userInfo),
+    };
+
+    const auth = {
+      encryptPassword: jest.fn().mockImplementation(() => userInfo),
+      generateToken: jest.fn().mockImplementation(() => 'tokenvalue'),
+    };
+
+    const user = await usersResolvers.signIn(null, userInfo, { userModel, auth });
+    expect(user).toEqual({
+      user: { ...userInfo },
+      token: 'tokenvalue',
+    });
+
+    expect(auth.generateToken).toHaveBeenCalled();
+    expect(auth.encryptPassword).toHaveBeenCalled();
+    expect(userModel.signIn).toHaveBeenCalled();
+  });
 });
