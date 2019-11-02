3e186e908291315d3ae2f0900dd91581ea6cf2ee
alejogs4 <alejogs4@gmail.com>
2019-11-02 16:53:58 -0500

    Corrige error en script de base de datos

--- database/database.sql
+++ database/database.sql
@@ -120,7 +120,7 @@ ALTER TABLE orders ADD COLUMN order_total FLOAT NOT NULL;
 -- -------------------------------------
 INSERT INTO categories(name) values('Producto General');
 ALTER TABLE products ADD COLUMN category_id int NOT NULL DEFAULT 1;
-ALTER TABLE products ADD CONSTRAINT fk_products_categories FPOREIGN KEY(category_id) REFERENCES categories(id);
+ALTER TABLE products ADD CONSTRAINT fk_products_categories FOREIGN KEY(category_id) REFERENCES categories(id);
 ALTER TABLE categories ADD CONSTRAINT uq_categories_name UNIQUE(name);
 ------------------------------------------
 ALTER TABLE users ADD COLUMN bond int not null default 0;
